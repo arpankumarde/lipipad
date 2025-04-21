196ba895c3cdc7c8b588d17dc68f105718011e3e
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-04-21 23:40:44 +0530

    basic code setup with nextjs, shadcn, supabase

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Lipipad",
+  description:
+    "LipiPad: An AI-powered notes app designed for seamless integration of your thoughts and tasks. Experience smart, intuitive, and efficient note-taking with LipiPad—where innovation meets simplicity.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
